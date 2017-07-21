c099164c3bca682a3cf993648ce8434d5dfc6fd5
David McFall <schorcher@schorcher.com>
2017-07-21 01:07:31 -0400

    Added Stuff

--- CRH/models.py
+++ CRH/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class User(models.Model):
+#     first_name = models.CharField(max_length=200)
+#     last_name = models.CharField(max_length=200)
+#     email = models.CharField(max_length=255)
+#     username = models.CharField(max_length=200)
+#     password = models.CharField(max_length=255)
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=255)
+
+
